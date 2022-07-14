f20fe74f78ac649025cda1462330b6963160ee66
KakuElvis <kakuelvis@gmail.com>
2022-07-14 18:02:46 +0000

    link tree temp

---
+++ partials/head.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content="Fruitask">
+    <title> Profile - Fruitask</title>
+    <link href="/css/uicons-regular-rounded.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+      tailwind.config = {
+        theme: {
+          extend: {
+            colors: {
+              'main': '#CCCC33',
+              'second-main': '#6666FF',
+              'text': '#1F2937  ', 
+              'background': '#E9EBF6  ', 
+            }
+          }
+        }
+      }
+    </script>
+</head>
+<body class="bg-background" >
