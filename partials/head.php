837749fcb0a6af4d5f6305853cbbdac9c488a5d3
KakuElvis <kakuelvis@gmail.com>
2022-07-17 18:35:48 +0000

    added icons folder

--- partials/head.php
+++ partials/head.php
@@ -6,7 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="title" content="Fruitask">
     <title> Profile - Fruitask</title>
-    <link href="/css/uicons-regular-rounded.css" rel="stylesheet">
+    <link href="./assets/flat/icon/v1/css/uicons-regular-rounded.css" rel="stylesheet">
+    <link href="./assets/flat/icon/v1/brands/css/uicons-brands.css" rel="stylesheet">
+    <link href="./assets/flat/icon/v1/solid/css/uicons-solid-rounded.css" rel="stylesheet">
+    <!-- <link href="https://fruitask.com/assets/flat/icon/v1/solid/css/uicons-solid-rounded.css" rel="stylesheet"> -->
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
       tailwind.config = {
--- profile_card.php
+++ profile_card.php
@@ -4,30 +4,38 @@
         <div class="relative container m-auto px-2 text-gray-500 md:px-12 xl:px-40">
             <div class="m-auto md:w-8/12 lg:w-10/12 xl:w-10/12">
                 <div class="rounded-xl bg-white shadow-xl">
-                    <div class="flex p-5 gap-2 relative">
-                        <img src="./img/mine.jpg" loading="lazy" class="w-10  rounded-full h-20 w-20  border border-4 border-second-main " alt="logo">
-                        <h2 class="py-5 text-2xl text-second-main font-bold">Kaku Elvis</h2>
+                    
+                    <div class="flex flex-col p-5 gap-2 relative justify-center items-center">
+                        <img src="./img/mine.jpg" loading="lazy" class="w-10  rounded-full h-20 w-20  border border-4 border-main " alt="logo">
+                        <h2 class=" text-2xl text-gray-600 font-bold">Kaku Elvis</h2>
+                        <h2 class=" text-lg text-main font-bold text-center">My FruitLinks</h2>
                     </div>
-                    <h2 class="px-5 text-2xl text-cyan-900 font-bold">My FruitLinks</h2>
-                    <div class="p-10 ">
+
+                    <div class="p-10 mx-4">
                         <div class="-mt-4 grid gap-2 sm:grid-cols-2 lg:grid-cols-3 md:-mx-8">
                             <div class="relative group">
                                 <a href="#">
                                     <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-background shadow-lg transition duration-300 group-hover:bg-main group-hover:scale-100 lg:group-hover:scale-100"></div>
                                     <div class="relative space-y-12 p-2 md:p-4 lg:p-6 xl:p-6">
                                         <div class="space-x-4 flex justify-center ">
-                                            <img src="./img/logo.png" class="w-10"   alt="">
-                                            <h4 class="text-xl text-gray-800 font-medium transition group-hover:text-white">Discord</h4>
+                                            <div class="justify-center items-center">
+                                            <span class="group-hover:text-white hover:text-white text-xl lg:text-2xl md:text-2xl"><i class="group-hover:text-white hover:text-white text-indigo-600  fi-brands-discord"></i></span>
+                                            </div>
+                                            <div>
+                                                <h4 class="text-xl text-gray-800 font-medium transition group-hover:text-white">Discord</h4>
+                                            </div>
                                         </div>
                                     </div>
                                 </a>
                             </div>
                             <div class="relative group">
                                 <a href="#">
-                                    <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-background shadow-lg transition duration-300 group-hover:bg-yellow-900 group-hover:scale-100 lg:group-hover:scale-100"></div>
+                                    <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-background shadow-lg transition duration-300 group-hover:bg-main group-hover:scale-100 lg:group-hover:scale-100"></div>
                                     <div class="relative space-y-12 p-2 md:p-12 lg:p-6 xl:p-6">
                                         <div class="space-x-4 flex justify-center">
-                                            <img src="./img/logo.png" class="w-10"  alt="">
+                                            <div>
+                                                <span class="group-hover:text-white hover:text-white text-xl lg:text-2xl md:text-2xl"><i class="group-hover:text-white hover:text-white text-indigo-600  fi-brands-facebook"></i></span>
+                                            </div>
                                             <h4 class="text-xl text-gray-800 font-medium transition group-hover:text-white">Facebook</h4>
                                         </div>
                                     </div>
@@ -35,10 +43,12 @@
                             </div>
                             <div class="relative group">
                                 <a href="#">
-                                    <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-background shadow-lg transition duration-300 group-hover:bg-yellow-900 group-hover:scale-100 lg:group-hover:scale-100"></div>
+                                    <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-background shadow-lg transition duration-300 group-hover:bg-main group-hover:scale-100 lg:group-hover:scale-100"></div>
                                     <div class="relative space-y-12 p-2 md:p-12 lg:p-6 xl:p-6">
                                         <div class="space-x-4 flex justify-center">
-                                            <img src="./img/logo.png" class="w-10"  alt="">
+                                            <div>
+                                                <span class="group-hover:text-white hover:text-white text-xl lg:text-2xl md:text-2xl"><i class="group-hover:text-white hover:text-white text-indigo-600  fi-brands-facebook-messenger"></i></span>
+                                            </div>
                                             <h4 class="text-xl text-gray-800 font-medium transition group-hover:text-white">Instagram</h4>
                                         </div>
                                     </div>
@@ -46,10 +56,12 @@
                             </div>
                             <div class="relative group ">
                                 <a href="#">
-                                    <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-background shadow-lg transition duration-300 group-hover:bg-yellow-900 group-hover:scale-100 lg:group-hover:scale-100"></div>
+                                    <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-background shadow-lg transition duration-300 group-hover:bg-main group-hover:scale-100 lg:group-hover:scale-100"></div>
                                     <div class="relative space-y-12 p-2 md:p-12 lg:p-4 xl:p-6">
                                         <div class="space-x-4 flex justify-center">
-                                            <img src="./img/logo.png" class="w-10"  alt="">
+                                            <div>
+                                                <span class="group-hover:text-white hover:text-white text-xl lg:text-2xl md:text-2xl"><i class="group-hover:text-white hover:text-white text-indigo-600  fi-brands-twitter"></i></span>
+                                            </div>
                                             <h4 class="text-xl text-gray-800 font-medium transition group-hover:text-white">Twitter</h4>
                                         </div>
                                     </div>
@@ -57,10 +69,12 @@
                             </div>
                             <div class="relative group ">
                                 <a href="#">
-                                    <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-background shadow-lg transition duration-300 group-hover:bg-yellow-900 group-hover:scale-100 lg:group-hover:scale-100"></div>
+                                    <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-background shadow-lg transition duration-300 group-hover:bg-main group-hover:scale-100 lg:group-hover:scale-100"></div>
                                     <div class="relative space-y-12 p-2 md:p-12 lg:p-4 xl:p-6">
                                         <div class="space-x-4 flex justify-center">
-                                            <img src="./img/logo.png" class="w-10"  alt="">
+                                            <div>
+                                                <span class="group-hover:text-white hover:text-white text-xl lg:text-2xl md:text-2xl"><i class="group-hover:text-white hover:text-white text-indigo-600  fi-brands-facebook-messenger"></i></span>
+                                            </div>
                                             <h4 class="text-xl text-gray-800 font-medium transition group-hover:text-white">Messenger</h4>
                                         </div>
                                     </div>
@@ -68,26 +82,33 @@
                             </div>
                             <div class="relative group ">
                                 <a href="#">
-                                    <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-background shadow-lg transition duration-300 group-hover:bg-yellow-900 group-hover:scale-105 lg:group-hover:scale-100"></div>
+                                    <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-background shadow-lg transition duration-300 group-hover:bg-main group-hover:scale-105 lg:group-hover:scale-100"></div>
                                     <div class="relative space-y-12 p-2 md:p-12 lg:p-4 xl:p-6">
                                         <div class="space-x-4 flex justify-center">
-                                            <img src="./img/logo.png" class="w-10"  alt="">
-                                            <h4 class="text-xl text-gray-800 font-medium transition group-hover:text-white">Youtube</h4>
+                                            <div>
+                                                <span class="group-hover:text-white hover:text-white text-xl lg:text-2xl md:text-2xl"><i class="group-hover:text-white hover:text-white text-indigo-600  fi-brands-youtube"></i></span>
+                                            </div>
+                                            <div>
+                                                <h4 class="text-xl text-gray-800 font-medium transition group-hover:text-white">Youtube</h4>
+                                            </div>
                                         </div>
                                     </div>
                                 </a> 
                             </div>
                         </div>
-
                         <div class="flex gap-2 items-center justify-center mt-8">
-                            <p class="text-center ralative -mb-1"> Powered by</p>
-                            <img src="https://fruitask.com/assets/logo/Fruitask-logo.svg" alt="Fruitask Logo" class="cursor-pointer h-5 w-auto">
+                            <p class="text-center  -mb-1"> Powered by</p>
+                            <a href="#">
+                                <img src="https://fruitask.com/assets/logo/Fruitask-logo.svg" alt="Fruitask Logo" class="h-5 w-auto">
+                            </a>
+                         
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    
                                 
 
 
